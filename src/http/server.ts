095e9af89b3754d417c0df04b034246a0cf21376
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2024-02-27 19:27:45 -0300

    refactor: split routes in files
    Remove routes in server.

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,42 +1,9 @@
 import chalk from 'chalk'
-import { Elysia, t } from 'elysia'
+import { Elysia } from 'elysia'
 
-import { db } from '../db/connection'
-import { restaurants, users } from '../db/schema'
+import { registerRestaurant } from './routes/register-restaurant'
 
-const app = new Elysia().post(
-  '/restaurants',
-  async ({ body, set }) => {
-    const { restaurantName, managerName, email, phone } = body
-
-    const [manager] = await db
-      .insert(users)
-      .values({
-        name: managerName,
-        email,
-        phone,
-        role: 'manager',
-      })
-      .returning({
-        id: users.id,
-      })
-
-    await db.insert(restaurants).values({
-      name: restaurantName,
-      managerId: manager.id,
-    })
-
-    set.status = 204
-  },
-  {
-    body: t.Object({
-      restaurantName: t.String(),
-      managerName: t.String(),
-      phone: t.String(),
-      email: t.String({ format: 'email' }),
-    }),
-  },
-)
+const app = new Elysia().use(registerRestaurant)
 
 app.listen(3333, () => {
   console.log(chalk.bgRed.black('🔥 HTTP Server running on port 3333'))
