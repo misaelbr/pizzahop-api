3bdf5d099a8c1f0985992cbcf37411a3427766ba
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2024-02-29 10:36:23 -0300

    feat: add support to passwordless auth
    This feature able the follow features:
    [x] support jwt token
    [x] support cookies
    [x] support auth from auth link

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,14 @@
 import chalk from 'chalk'
 import { Elysia } from 'elysia'
 
+import { authenticateFromLink } from './routes/authenticate-from-link'
 import { registerRestaurant } from './routes/register-restaurant'
+import { sendAuthLink } from './routes/send-auth-link'
 
-const app = new Elysia().use(registerRestaurant)
+const app = new Elysia()
+  .use(registerRestaurant)
+  .use(sendAuthLink)
+  .use(authenticateFromLink)
 
 app.listen(3333, () => {
   console.log(chalk.bgRed.black('🔥 HTTP Server running on port 3333'))
