86141a20d8b575b1fb61bfda70aa65b03eaa2516
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2024-02-29 10:39:28 -0300

    feat: add support to logout
    Created route to user logout

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,11 +4,13 @@ import { Elysia } from 'elysia'
 import { authenticateFromLink } from './routes/authenticate-from-link'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
+import { signOut } from './routes/sign-out'
 
 const app = new Elysia()
   .use(registerRestaurant)
   .use(sendAuthLink)
   .use(authenticateFromLink)
+  .use(signOut)
 
 app.listen(3333, () => {
   console.log(chalk.bgRed.black('🔥 HTTP Server running on port 3333'))
