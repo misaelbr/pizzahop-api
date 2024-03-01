cf93c8d60b730e09cd10e9636a971fdb105ad251
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2024-02-29 21:26:45 -0300

    feat: add route to get managed restaurant

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,6 +2,7 @@ import chalk from 'chalk'
 import { Elysia } from 'elysia'
 
 import { authenticateFromLink } from './routes/authenticate-from-link'
+import { getManagedRestaurant } from './routes/get-managed-restaurant'
 import { getProfile } from './routes/get-profile'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
@@ -13,6 +14,7 @@ const app = new Elysia()
   .use(authenticateFromLink)
   .use(signOut)
   .use(getProfile)
+  .use(getManagedRestaurant)
 
 app.listen(3333, () => {
   console.log(chalk.bgRed.black('🔥 HTTP Server running on port 3333'))
