1e9a2160a06749654cfedd5c12590048342962f0
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2024-02-29 21:09:27 -0300

    feat: add support to view profile

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -17,7 +17,7 @@ export const auth = new Elysia()
     }),
   )
   .use(cookie())
-  .derive(({ jwt, setCookie, removeCookie }) => {
+  .derive(({ jwt, setCookie, removeCookie, cookie }) => {
     return {
       signUser: async (payload: Static<typeof jtwPayload>) => {
         const token = await jwt.sign(payload)
@@ -30,6 +30,20 @@ export const auth = new Elysia()
       },
       signOut: () => {
         removeCookie('auth')
+      },
+
+      getCurrentUser: async () => {
+        const authCookie = cookie.auth
+
+        const payload = await jwt.verify(authCookie)
+
+        if (!payload) {
+          throw new Error('Unauthorized')
+        }
+        return {
+          userId: payload.sub,
+          restaurantId: payload.restaurantId,
+        }
       },
     }
   })
--- src/http/server.ts
+++ src/http/server.ts
@@ -2,6 +2,7 @@ import chalk from 'chalk'
 import { Elysia } from 'elysia'
 
 import { authenticateFromLink } from './routes/authenticate-from-link'
+import { getProfile } from './routes/get-profile'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
 import { signOut } from './routes/sign-out'
@@ -11,6 +12,7 @@ const app = new Elysia()
   .use(sendAuthLink)
   .use(authenticateFromLink)
   .use(signOut)
+  .use(getProfile)
 
 app.listen(3333, () => {
   console.log(chalk.bgRed.black('🔥 HTTP Server running on port 3333'))
