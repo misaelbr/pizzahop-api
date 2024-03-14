6a21d63fadfa30c0d7f08efefdfbba707ac1c796
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2024-03-13 22:53:30 -0300

    feat: add relations in entities

--- src/db/schema/products.ts
+++ src/db/schema/products.ts
@@ -1,5 +1,8 @@
 import { createId } from '@paralleldrive/cuid2'
+import { relations } from 'drizzle-orm'
 import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+
+import { orderItems, restaurants } from '.'
 
 export const products = pgTable('products', {
   id: text('id')
@@ -9,6 +12,22 @@ export const products = pgTable('products', {
   name: text('name').notNull(),
   description: text('description'),
   priceInCents: integer('price_in_cents').notNull(),
+  restaurantId: text('restaurant_id')
+    .notNull()
+    .references(() => restaurants.id, {
+      onDelete: 'cascade',
+    }),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at').notNull().defaultNow(),
+})
+
+export const productsRelations = relations(products, ({ one, many }) => {
+  return {
+    restaurant: one(restaurants, {
+      fields: [products.restaurantId],
+      references: [restaurants.id],
+      relationName: 'order_restaurant',
+    }),
+    orderItems: many(orderItems),
+  }
 })
