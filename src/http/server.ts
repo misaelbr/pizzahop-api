6adf64b85491b768fd8c462cf42f0e64247ba835
Misael Bandeira Silveira <misael.bandeira@gmail.com>
2024-03-21 12:15:00 -0300

    feat: add getOrderDetails

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,6 +3,7 @@ import { Elysia } from 'elysia'
 
 import { authenticateFromLink } from './routes/authenticate-from-link'
 import { getManagedRestaurant } from './routes/get-managed-restaurant'
+import { getOrderDetails } from './routes/get-orders-details'
 import { getProfile } from './routes/get-profile'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
@@ -15,6 +16,7 @@ const app = new Elysia()
   .use(signOut)
   .use(getProfile)
   .use(getManagedRestaurant)
+  .use(getOrderDetails)
   .onError(({ error, code, set }) => {
     switch (code) {
       case 'VALIDATION': {
